efa08b35a2119e9d8e9d7c9a199533e458749403
jukmifgguggh1337 <spamandjunkmail9@gmail.com>
2025-08-09 14:27:42 -0600

    enemy death anim

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,38 +1,59 @@
 extends CharacterBody2D
 
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+@onready var sprite_origin: Node2D = $SpriteOrigin
+@onready var animated_sprite_2d: AnimatedSprite2D = $SpriteOrigin/AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 const ACCEL = 300.0
 
-const MAX_HEALTH: int = 20
+const MAX_HEALTH: int = 15
 var health: int = MAX_HEALTH
 
-@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var sprite_origin: Node2D = $SpriteOrigin
+const SLIDING_DECEL = 400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	var direction = sign((player.position - position).x)
-	if direction:
-		velocity.x = move_toward(velocity.x, direction * SPEED, ACCEL * delta)
-		sprite_origin.scale.x = -abs(sprite_origin.scale.x) * direction
+	if health > 0:
+		var direction = sign((player.position - position).x)
+		if direction:
+			velocity.x = move_toward(velocity.x, direction * SPEED, ACCEL * delta)
+			sprite_origin.scale.x = -abs(sprite_origin.scale.x) * direction
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, SLIDING_DECEL * delta)
+
+	handle_sprite()
 
 	move_and_slide()
 
-const X_KNOCKBACK = 300.0
+const MAX_HURT_FRAMES = 20
+var hurt_frames: int
+func handle_sprite() -> void:
+	if health <= 0:
+		if animated_sprite_2d.animation != "die":
+			animated_sprite_2d.play("die")
+	elif hurt_frames > 0:
+		animated_sprite_2d.play("hurt")
+		hurt_frames -= 1
+	else:
+		animated_sprite_2d.play("run")
+
+const X_KNOCKBACK = 250.0
 const Y_KNOCKBACK = -300.0
 func take_damage(bucket: RigidBody2D, damage: int) -> void:
+	# Knockback
+	velocity = Vector2(sign(position.x - player.position.x) * X_KNOCKBACK, Y_KNOCKBACK)
+	# Decrease health
 	health -= damage
 	if health <= 0:
-		die()
+		animation_player.play("die")
 	else:
-		velocity = Vector2(sign(position.x - player.position.x) * X_KNOCKBACK, Y_KNOCKBACK)
-
-func die() -> void:
-	queue_free()
+		hurt_frames = MAX_HURT_FRAMES
